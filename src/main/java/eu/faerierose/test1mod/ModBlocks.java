bd342072b7d76c46b07eab19166278199c03dab0
FaerieRose <rosalynn5772-git@yahoo.com>
2017-10-18 21:08:07 +0200

    new png files

--- src/main/java/eu/faerierose/test1mod/ModBlocks.java
+++ src/main/java/eu/faerierose/test1mod/ModBlocks.java
@@ -7,9 +7,11 @@ import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 	public static Block test1Block = new Test1ModBlock("faerie_rose_test1Block", Material.iron);
+	public static Block test2Block = new Test1ModBlock("faerie_rose_test2Block", Material.cloth);
 	
 	public static void init() {
 		RegisterHelper.registerBlock(test1Block);
+		RegisterHelper.registerBlock(test2Block);
 	}
 
 }
